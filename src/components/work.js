623f7e8cb47748cc56a8c64a1cef6660e6b3f64d
FPaurisse <paurissefrederic@gmail.com>
2020-02-27 21:01:41 +0100

    import data

--- src/components/work.js
+++ src/components/work.js
@@ -12,7 +12,7 @@ const Work = ({ data }) => {
       <div className={workStyles.image} style={{ backgroundImage: `url(${image})` }} />
       <div className={workStyles.back} style={{ backgroundColor: data.optionalColor }} />
       <div className={workStyles.backGradient} style={{ backgroundImage: `linear-gradient(45deg, ${data.primaryColor} 15%, ${data.secondaryColor} 70%)` }} />
-      <h1 className={workStyles.title} style={{ backgroundImage: `url(${data.image})` }}>{data.title}</h1>
+      <h1 className={workStyles.title} style={{ backgroundImage: `url(${image})` }}>{data.title}</h1>
       <div className={workStyles.mockup} style={{ backgroundImage: `url(${mockup})` }} />
     </Link>
   );
