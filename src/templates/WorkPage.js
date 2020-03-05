a95bcdbdb8fb4887a392bbe6ba8208e3a92e8925
FPaurisse <paurissefrederic@gmail.com>
2020-03-05 08:38:20 +0100

    add view box template

--- src/templates/WorkPage.js
+++ src/templates/WorkPage.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { graphql } from 'gatsby';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
+import s from '../styles/WorkPage.module.css';
+
+const baseURL = process.env.GATSBY_API_URL || '';
 
 export default ({ data }) => {
   const work = data.restApiApiV1Works;
@@ -9,7 +12,40 @@ export default ({ data }) => {
   return (
     <Layout>
       <SEO title={work.title} description={work.context} />
-      <div>
+      <div className={s.WorkPage}>
+        <div className={s.view}>
+          <div
+            className={s.image}
+            style={{
+              backgroundImage: `url(${work.image.includes(baseURL)
+                ? work.image
+                : baseURL + work.image})`,
+            }}
+          />
+          <div className={s.back} style={{ backgroundColor: `${work.optionalColor}` }} />
+          <div className={s.backGradient} style={{ backgroundImage: `linear-gradient(45deg, ${work.primaryColor} 15%, ${work.secondaryColor} 70%)` }} />
+          <h1
+            className={s.title}
+            style={{
+              backgroundImage: `url(${work.image.includes(baseURL)
+                ? work.image
+                : baseURL + work.image})`,
+            }}
+          >
+            {work.title}
+
+          </h1>
+          <div
+            className={s.mockup}
+            style={{
+              backgroundImage: `url(${work.mockup.includes(baseURL)
+                ? work.mockup
+                : baseURL + work.mockup})`,
+              width: `${work.width}%`,
+              height: `${work.height}%`,
+            }}
+          />
+        </div>
         <h1>{work.title}</h1>
         <p>{work.context}</p>
         <div>{work.tools.map((tool) => <h4>{tool}</h4>)}</div>
@@ -22,10 +58,18 @@ export default ({ data }) => {
 export const query = graphql`
   query($slug: String!) {
     restApiApiV1Works(slug: {eq: $slug}) {
-          categories
-          title
-          context
-          tools
+      endpointId
+      title
+      context
+      tools
+      categories
+      primaryColor
+      secondaryColor
+      optionalColor
+      image
+      mockup
+      width
+      height
     }
   }
 `;
