f433859855c194d121c8673be5401eca5947e2aa
FPaurisse <paurissefrederic@gmail.com>
2020-03-08 10:11:31 +0100

    add maintenance page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
+import s from '../styles/page.module.css';
 
 const About = ({ location }) => (
   <Layout location={location}>
     <SEO title="About me" />
-    <h1>About me</h1>
+    <div className={s.Page}>
+      <p>Coming soon...</p>
+    </div>
   </Layout>
 );
 
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
+import s from '../styles/page.module.css';
 
 const Contact = ({ location }) => (
   <Layout location={location}>
     <SEO title="Contact me" />
-    <h1>Contact me</h1>
+    <div className={s.Page}>
+      <p>Coming soon...</p>
+    </div>
   </Layout>
 );
 
