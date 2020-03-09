bf591f2b44e3c405fe59ce0239e56eaa37e694df
FPaurisse <paurissefrederic@gmail.com>
2020-03-09 20:53:18 +0100

    add logo

--- src/components/header.js
+++ src/components/header.js
@@ -2,31 +2,35 @@ import React from 'react';
 import { Link } from 'gatsby';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
+import { faHatCowboy } from '@fortawesome/free-solid-svg-icons';
 import cx from 'classnames';
-import headerStyles from '../styles/header.module.css';
+import s from '../styles/header.module.css';
+import Logo from './logo';
 
 const Header = ({ siteTitle, location }) => (
-  <header className={headerStyles.Header}>
-    <Link to="/">
-      <div className={headerStyles.logo} title={siteTitle} />
+  <header className={s.Header}>
+    <Link className={s.logoWrapper} to="/">
+      <div className={s.logo} title={siteTitle}>
+        <div className={s.iconLogo}>
+          <Logo />
+        </div>
+      </div>
+      <h1 className={s.title}>Creative developer</h1>
     </Link>
-    <nav className={headerStyles.nav}>
-      <ul className={headerStyles.list}>
-        <li className={headerStyles.item}>
-          <Link to="/" activeClassName={headerStyles.linkActive} className={cx(location.pathname.includes('works') && headerStyles.linkActive, headerStyles.link)}>Works</Link>
+    <nav className={s.nav}>
+      <ul className={s.list}>
+        <li className={s.item}>
+          <Link to="/" activeClassName={s.linkActive} className={cx(location.pathname.includes('works') && s.linkActive, s.link)}>Works</Link>
         </li>
-        <li className={headerStyles.item}>
-          <Link to="/about" activeClassName={headerStyles.linkActive} className={headerStyles.link}>About</Link>
-        </li>
-        <li className={headerStyles.item}>
-          <Link to="/contact" activeClassName={headerStyles.linkActive} className={headerStyles.link}>Contact</Link>
+        <li className={s.item}>
+          <Link to="/contact" activeClassName={s.linkActive} className={s.link}>Contact</Link>
         </li>
       </ul>
     </nav>
-    <div className={headerStyles.social}>
-      <div className={headerStyles.text}>Follow me</div>
-      <div className={headerStyles.social__wrapper}>
-        <a className={headerStyles.social__link} href="https://www.linkedin.com/in/frederic-paurisse/" rel="noopener noreferrer" target="_blank">
+    <div className={s.social}>
+      <div className={s.text}>Follow me</div>
+      <div className={s.social__wrapper}>
+        <a className={s.social__link} href="https://www.linkedin.com/in/frederic-paurisse/" rel="noopener noreferrer" target="_blank">
           <FontAwesomeIcon icon={faLinkedinIn} />
         </a>
       </div>
