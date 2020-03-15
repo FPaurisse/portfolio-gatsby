256a92d725e1da46350a739bf328fdb925ffca3a
FPaurisse <paurissefrederic@gmail.com>
2020-03-15 21:56:15 +0100

    add contact styles

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+import cx from 'classnames';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faComments, faPaperPlane,
+} from '@fortawesome/free-solid-svg-icons';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
-import s from '../styles/page.module.css';
+import s from '../styles/contact.module.css';
+import contact from '../images/contact.jpg';
 
 const Contact = ({ location }) => (
   <Layout location={location}>
@@ -9,9 +16,43 @@ const Contact = ({ location }) => (
       title="Contact"
       description="Développeur web et webdesigner freelance à Tours (37), je développe pour vous tout type de projets web et print : sites internet, applications web, identités visuelles et chartes graphiques."
     />
-    <div className={s.Page}>
-      <p>Coming soon...</p>
-      <a href="mailto:contact@fredericpaurisse.fr">contact@fredericpaurisse.fr</a>
+    <div className={s.Contact}>
+      <div className={s.wrapper}>
+        <div
+          className={s.media}
+          style={{
+            backgroundImage: `linear-gradient(black, black), url(${contact})`,
+          }}
+        >
+          <div className={cx(s.mediaWrapper)} style={{ borderImageSource: 'linear-gradient(45deg, #33B0CB 15%, #3a3837 70%)' }}>
+            <div className={s.optionalBack} style={{ backgroundColor: '#ffffff' }}>
+              <div className={s.backGradient} style={{ backgroundImage: 'linear-gradient(45deg, #33B0CB 15%, #3a3837 70%)' }} />
+            </div>
+          </div>
+        </div>
+        <form
+          className={s.form}
+        >
+          <h1 className={s.title}>Contact</h1>
+          <p className={s.text}>hello@fredericpaurisse.fr</p>
+          <div className={s.inputGroup}>
+            <input required className={s.input} type="text" placeholder="Firstname" />
+            <input required className={s.input} type="text" placeholder="Lastname" />
+          </div>
+          <div className={s.inputSimple}>
+            <input required className={s.input} type="text" placeholder="Email" />
+          </div>
+          <div className={s.inputSimple}>
+            <textarea required className={s.input} cols="30" rows="10" placeholder="Message" />
+          </div>
+          <div className={s.inputSimple}>
+            <button className={cx(s.button)} type="submit">
+              <FontAwesomeIcon className={s.iconButton} icon={faPaperPlane} />
+              Send
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   </Layout>
 );
