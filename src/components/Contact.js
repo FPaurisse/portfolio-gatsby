aef4d8b9f4fa859d29b27e00d6bbb88926a02d3c
FPaurisse <paurissefrederic@gmail.com>
2020-03-18 09:56:03 +0100

    add social block and close contact button

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,14 +4,21 @@ import cx from 'classnames';
 import { connect } from 'react-redux';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
-  faPaperPlane,
+  faPaperPlane, faAt, faTimesCircle,
 } from '@fortawesome/free-solid-svg-icons';
+import {
+  faFacebookMessenger, faDiscord,
+} from '@fortawesome/free-brands-svg-icons';
 import s from '../styles/contact.module.css';
 
 import { toggleContact } from '../state/app';
 
 const Contact = ({ isContact, dispatch }) => (
   <div className={cx({ [s.Contact]: isContact, [s.Contact__hide]: !isContact })}>
+    <button type="button" className={s.closeContact} onClick={() => dispatch(toggleContact(false))}>
+      <FontAwesomeIcon className={s.iconCloseContact} icon={faTimesCircle} />
+      Close
+    </button>
     <form
       className={s.form}
     >
@@ -35,6 +42,17 @@ const Contact = ({ isContact, dispatch }) => (
         </button>
       </div>
     </form>
+    <div className={s.social}>
+      <div className={s.socialWrapper}>
+        <FontAwesomeIcon className={s.socialIcon} icon={faFacebookMessenger} />
+      </div>
+      <div className={s.socialWrapper}>
+        <FontAwesomeIcon className={s.socialIcon} icon={faDiscord} />
+      </div>
+      <div className={s.socialWrapper}>
+        <FontAwesomeIcon className={s.socialIcon} icon={faAt} />
+      </div>
+    </div>
   </div>
 );
 
