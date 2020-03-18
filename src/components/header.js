85df16ecbf8a304594090261be7af3917aeb7758
FPaurisse <paurissefrederic@gmail.com>
2020-03-18 12:26:40 +0100

    ever close terms

--- src/components/header.js
+++ src/components/header.js
@@ -7,40 +7,48 @@ import cx from 'classnames';
 import s from '../styles/header.module.css';
 import Logo from './logo';
 
-import { toggleContact } from '../state/app';
+import { toggleContact, toggleTerms } from '../state/app';
 
-const Header = ({
-  isContact, dispatch, siteTitle,
-}) => (
-  <header className={s.Header}>
-    <Link className={s.logoWrapper} to="/" onClick={() => dispatch(toggleContact(false))}>
-      <div className={s.logo} title={siteTitle}>
-        <div className={s.iconLogo}>
-          <Logo />
+const Header = ({ isContact, dispatch, siteTitle }) => {
+  const closeModal = () => {
+    dispatch(toggleContact(false));
+    dispatch(toggleTerms(false));
+  };
+  const handleContact = () => {
+    dispatch(toggleContact(!isContact));
+    dispatch(toggleTerms(false));
+  };
+  return (
+    <header className={s.Header}>
+      <Link className={s.logoWrapper} to="/" onClick={() => closeModal()}>
+        <div className={s.logo} title={siteTitle}>
+          <div className={s.iconLogo}>
+            <Logo />
+          </div>
+        </div>
+        <h1 className={s.title}>Creative developer</h1>
+      </Link>
+      <nav className={s.nav}>
+        <ul className={s.list}>
+          <li className={s.item}>
+            <Link to="/" onClick={() => closeModal()} className={cx(s.link, { [s.linkActive]: !isContact })}>Works</Link>
+          </li>
+          <li className={s.item}>
+            <button type="button" onClick={() => handleContact()} className={cx(s.link, { [s.linkActive]: isContact })}>Contact</button>
+          </li>
+        </ul>
+      </nav>
+      <div className={s.social}>
+        <div className={s.text}>Follow me</div>
+        <div className={s.social__wrapper}>
+          <a className={s.social__link} href="https://www.linkedin.com/in/frederic-paurisse/" rel="noopener noreferrer" target="_blank">
+            <FontAwesomeIcon icon={faLinkedinIn} />
+          </a>
         </div>
       </div>
-      <h1 className={s.title}>Creative developer</h1>
-    </Link>
-    <nav className={s.nav}>
-      <ul className={s.list}>
-        <li className={s.item}>
-          <Link to="/" onClick={() => dispatch(toggleContact(false))} className={cx(s.link, { [s.linkActive]: !isContact })}>Works</Link>
-        </li>
-        <li className={s.item}>
-          <button type="button" onClick={() => dispatch(toggleContact(!isContact))} className={cx(s.link, { [s.linkActive]: isContact })}>Contact</button>
-        </li>
-      </ul>
-    </nav>
-    <div className={s.social}>
-      <div className={s.text}>Follow me</div>
-      <div className={s.social__wrapper}>
-        <a className={s.social__link} href="https://www.linkedin.com/in/frederic-paurisse/" rel="noopener noreferrer" target="_blank">
-          <FontAwesomeIcon icon={faLinkedinIn} />
-        </a>
-      </div>
-    </div>
-  </header>
-);
+    </header>
+  );
+};
 
 export default connect((state) => ({
   isContact: state.app.isContact,
