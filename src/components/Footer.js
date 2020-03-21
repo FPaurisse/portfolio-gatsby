e0f3cb22fe73d02c19c49d087c209e0dda5ca74c
FPaurisse <paurissefrederic@gmail.com>
2020-03-21 09:42:41 +0100

    add Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import s from '../styles/Footer.module.css';
 const Footer = () => (
   <div className={s.Footer}>
     <p className={s.text}>
-      © Frédéric Paurisse 2020 - Build with
+      © Frédéric Paurisse 2020 - Built with
       {' '}
       <a className={s.link} rel="noopener noreferrer" target="_blank" href="https://www.gatsbyjs.org/">Gatsby</a>
     </p>
