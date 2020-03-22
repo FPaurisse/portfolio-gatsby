256059dacb0f140dcf0338a2fae4a0448cb951f4
FPaurisse <paurissefrederic@gmail.com>
2020-03-22 17:33:54 +0100

    fix

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,7 +5,7 @@ import SEO from '../components/SEO/SEO';
 const NotFoundPage = ({ location }) => (
   <PageLayout location={location}>
     <SEO title="404: Not found" />
-    <div className={s.Page}>
+    <div>
       <h1>NOT FOUND</h1>
       <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
     </div>
