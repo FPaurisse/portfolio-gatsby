642427edd4fda891734d5400e237037692858a93
FPaurisse <paurissefrederic@gmail.com>
2020-03-23 13:19:33 +0100

    fix red warning

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -2,9 +2,6 @@ import React from 'react';
 
 const Logo = () => (
   <svg
-    id="Calque_1"
-    data-name="Calque 1"
-    xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 240 232.23"
   >
     <polygon
